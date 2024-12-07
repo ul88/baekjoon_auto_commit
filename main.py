63ff69931dc8807eb0acbd17506946bc6ee7091f
ul88 <hkimog13@naver.com>
2024-12-07 22:42:57 +0900

    api로 수정

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import fnmatch
 import shutil
 import subprocess
 
-PS_REPO_PATH = "C:\\Users\\hkimo\\OneDrive\\Desktop\\coding\\C++\\PS\\boj"
-GIT_REPO_PATH = "C:\\Users\\hkimo\\github_repo\\problem-solving"
+PS_REPO_PATH = "[복사할 데이터]"
+GIT_REPO_PATH = "[복사할 데이터가 갈 장소]"
 
-url = 'https://solved.ac/search?query='
+url = 'https://solved.ac/api/v3/problem/show?problemId='
+
+dict_level = {0: "Unrated", 1: "Bronze V", 2: "Bronze IV", 3: "Bronze III", 4: "Bronze II", 5: "Bronze I",
+                6: "Silver V", 7: "Silver IV", 8: "Silver III", 9: "Silver II", 10: "Silver I",
+                11: "Gold V", 12: "Gold IV", 13: "Gold III", 14: "Gold II", 15: "Gold I",
+                16: "Platinum V", 17: "Platinum IV", 18: "Platinum III", 19: "Platinum II", 20: "Platinum I",
+                21: "Diamond V", 22: "Diamond IV", 23: "Diamond III", 24: "Diamond II", 25: "Diamond I",
+                26: "Ruby V", 27: "Ruby IV", 28: "Ruby III", 29: "Ruby II", 30: "Ruby I"}
 
 def git_pull(remote='origin', branch='main'):
     try:
@@ -38,21 +45,13 @@ def git_commit_push(commit_message):
         print(f"오류 발생: {e}")
 
 def check_level(number):
-    response = requests.get(url + number)  # GET 메소드를 사용하여 url에 HTTP Request 전송
+    response = requests.get(url + number)
     if response.status_code == 200:  # 정상 응답 반환 시 아래 코드블록 실행
-        soup = BeautifulSoup(response.content, 'html.parser')  # 응답 받은 HTML 파싱
-        a_tag = soup.find('a', class_='css-q9j30p', href="https://www.acmicpc.net/problem/" + number)
-
-        if a_tag:
-            images = soup.find_all('img', class_="css-1vnxcg0")
-            return images[0].get('alt')
-        else:
-            print("not found")
-    else:
-        print('error')  # 오류 시 메시지 출력
+        return dict_level[response.json().get("level")]
+    
     return "ERROR"
 
-def upload():
+def upload_all():
     flag = False
     cnt = 0
 
@@ -71,7 +70,8 @@ def upload():
                 os.makedirs(now_path)
             
             if file in os.listdir(now_path):
-                print("존재합니다")
+                #print("존재합니다")
+                pass
             else:
                 flag = True
                 cnt += 1
@@ -83,6 +83,4 @@ def upload():
     else:
         print("커밋 & 푸시를 진행하지 않습니다.")
 
-upload()
-    
-        
+upload_all()
